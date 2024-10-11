e27b89bc3688052c988fcb59e54f8489c57c58f2
Michael Guevara <mebguevara@addu.edu.ph>
2024-10-11 11:51:54 +0800

    user card

--- lib/home.dart
+++ lib/home.dart
@@ -52,58 +52,140 @@ class HomePages extends State<HomePage> {
                 image: DecorationImage(
                   image: const ExactAssetImage('assets/climb.jpg'),
                   fit: BoxFit.cover,
-                  colorFilter: ColorFilter.mode(const Color.fromARGB(255, 86, 132, 170).withOpacity(0.9), BlendMode.multiply),
+                  colorFilter: ColorFilter.mode(const Color.fromARGB(255, 118, 183, 236).withOpacity(0.9), BlendMode.multiply),
                 )
               ),
             ),
            Center(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Image(
-                  image: ExactAssetImage('assets/logo.png')
+                  image: ExactAssetImage('assets/logo.png'),
+                  width: 100,
+                  height: 100,
                 ),
                 person.isEmpty ? loadingIndicator() : 
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 245, 251, 255)
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(
-                        child: CircleAvatar(
-                          radius: 50,
-                          backgroundImage: NetworkImage(person[0]['picture']['large']),  
-                                          
+                Stack(
+                  children: [
+                    Center(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width*.92,
+                        height: MediaQuery.of(context).size.height*.3,
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(5)
                         ),
                       ),
-                      const SizedBox(height: 16),
-                      Text(
-                        '${person[0]['name']['first']}',
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                    ),
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 55, 0, 0),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width*.92,
+                          height: MediaQuery.of(context).size.height*.30,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)
+                          ),
                         ),
                       ),
-                      const SizedBox(height: 1),  
-                  
-                      Text(
-                        '${person[0]['name']['last']}',
-                        style: const TextStyle(
-                          fontSize: 16,  
-                          color: Colors.grey,
-                        ),
+                    ),
+                    Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width*.92,
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Center(
+                                  child: CircleAvatar(
+                                    radius: 55,
+                                    backgroundColor: Colors.white,
+                                    child: CircleAvatar(
+                                      radius: 50,
+                                      backgroundImage: NetworkImage(person[0]['picture']['large']),      
+                                    ),
+                                  ),
+                              ),
+                              Text(
+                                '${person[0]['name']['first']} ${person[0]['name']['last']}',
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.location_on_outlined,
+                                    color: Colors.grey,
+                                    size: 12,
+                                  ),
+                                  Text(
+                                    ' ${person[0]['location']['city']}, ${person[0]['location']['country']}',
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const Divider(
+                                color: Colors.grey,
+                                indent: 20,
+                                endIndent: 20,
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                                child:  Text('I am a Car'),
+                              ),
+                              const Divider(
+                                color: Colors.grey,
+                                indent: 40,
+                                endIndent: 40,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.email_outlined,
+                                    color: Colors.grey,
+                                    size: 12,
+                                  ),
+                                  Text(
+                                    ' ${person[0]['email']}',
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Icon(
+                                    Icons.phone_android,
+                                    color: Colors.grey,
+                                    size: 12,
+                                  ),
+                                  Text(
+                                    ' ${person[0]['phone']}',
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                       ),
-                      const SizedBox(height: 1),
-                      Text(
-                        '${person[0]['email']}',
-                        style: const TextStyle(
-                          fontSize: 16,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
 
                 TextButton(
