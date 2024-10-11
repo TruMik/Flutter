2280aed57a805cb3f2d57c010ca44fddf7337377
Michael Guevara <mebguevara@addu.edu.ph>
2024-10-11 09:04:06 +0800

    1

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,17 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 class HomePage extends StatefulWidget {
   @override
-  HomePages createState() => HomePages();
+  State<HomePage> createState() => HomePages();
 }
 
 class HomePages extends State<HomePage> {
-  List meals = []
-  bool isLoading = true;
   Dio dio = Dio();
-  final url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=chicken';
+  final url = 'https://randomuser.me/api/';
+  List person = [];
 
   @override
   void initState() {
@@ -20,13 +21,24 @@ class HomePages extends State<HomePage> {
 
   Future<void> fetchPosts() async {
     try {
-      setState(() {
-        meals = response.data['meals'];
-        isLoading = false;
-      });
+      var response = await Dio().get(url);
+      if(response.statusCode == 200){
+        setState(() {
+          person = response.data['results'];
+        });
+      }
+      else{
+        print(response.statusCode);
+      }
     } catch (e) {
       print('Error occurred: $e');
     }
+  }
+
+  Widget loadingIndicator() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
   }
 
   @override
@@ -34,46 +46,86 @@ class HomePages extends State<HomePage> {
     return Scaffold(
       body: Stack(
         children: [
-          Column(
-            children: [
-              Container(
-                height: MediaQuery.of(context).size.height,
-                decoration: const BoxDecoration(
-                  color: Color.fromARGB(255, 255, 255, 255)
-                ),
+          Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const ExactAssetImage('assets/climb.jpg'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(const Color.fromARGB(255, 86, 132, 170).withOpacity(0.9), BlendMode.multiply),
+                )
               ),
-            ],
-          ),
+            ),
            Center(
             child: Column(
               children: [
                 const Image(
                   image: ExactAssetImage('assets/logo.png')
                 ),
-                TextButton(
-                onPressed: () {
-                  Navigator.popUntil(context, ModalRoute.withName('/login'));
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(0),
-                    side: BorderSide.none
+                person.isEmpty ? loadingIndicator() : 
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 245, 251, 255)
                   ),
-                  backgroundColor: Colors.transparent, 
-                  elevation: 0,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: NetworkImage(person[0]['picture']['large']),  
+                                          
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        '${person[0]['name']['first']}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 1),  
                   
-                ),
-                child: const Text(
-                  "LOGOUT",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.grey,
+                      Text(
+                        '${person[0]['name']['last']}',
+                        style: const TextStyle(
+                          fontSize: 16,  
+                          color: Colors.grey,
+                        ),
+                      ),
+                      const SizedBox(height: 1),
+                      Text(
+                        '${person[0]['email']}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                ),
-                ElevatedButton(
-                  onPressed: fetchPosts,
-                  child: Text('Fetch Posts'),
+
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      side: BorderSide.none
+                    ),
+                    backgroundColor: Colors.transparent, 
+                    elevation: 0,
+                    
+                  ),
+                  child: const Text(
+                    "LOGOUT",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.grey,
+                    ),
+                  ),
                 ),
               ],
             ),
